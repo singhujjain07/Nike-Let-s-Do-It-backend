0c0aafbd86fa6c5199fb5149bdb313e48adfe1a3
Harsh Singh <harshsinghujjain07@gmail.com>
2024-06-04 16:13:03 +0530

    updates

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,7 +12,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 export const placeOrderController = async (req, res) => {
-    const frontend_url = 'http://localhost:5173'
+    const frontend_url = 'https://nike-revamped.netlify.app'
     try {
         const {userId,items,amount,address} = req.body;
         const newOrder = new orderModel({userId,items,amount,address});
